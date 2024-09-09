adfe6df59f4065efda44d32e2191e58d54bcd3d9
thalistornquist <thalistornquist@gmail.com>
2024-09-09 19:56:29 -0300

    if / else

---
+++ aula011/ex008.js
@@ -0,0 +1,5 @@
+var vel = 61
+console.log(`A velocidade de seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log('Voce esta acima da velocidade permitida, por isso foi multado!')
+}// else (console.log('Dirija com cuidado e use cinto de segurança.'))
---
+++ aula011/ex009.js
@@ -0,0 +1,8 @@
+var país = 'EUA'
+var BR = 'Brasil'
+console.log(`Vivendo no ${BR}`)
+if (país == 'Brasil') {
+    console.log('Voce é Brasileiro!')
+} else {
+    console.log('Voce é Gringo!')
+}
