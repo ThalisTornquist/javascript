ab4d0cc23ae19ef63ce81fd0d494f44f42e4cd08
thalistornquist <thalistornquist@gmail.com>
2024-09-15 15:35:10 -0300

    fim dos exerciocios

--- aula013/js/script.js
+++ aula013/js/script.js
@@ -7,26 +7,26 @@ var manha = document.getElementById('f1')
 var tarde = document.getElementById('f2')
 var noite = document.getElementById('f3')
 
+
+hora = 13
 if (hora > 3 && hora <= 12){
     document.body.style.backgroundColor = 'rgb(211, 185, 132)'
     turno = 'manha'
     res.innerHTML = `Agora são <strong>${hora}</strong> horas da <strong>${turno}.</strong>`
+
+    manha.setAttribute('src', 'imagens/manha.jpg')
 } else if(hora <= 18 && hora > 12){
-    document.body.style.backgroundColor = '#c55412'
+    document.body.style.backgroundColor = '#ed8b5c'
     turno = 'tarde'
     res.innerHTML = `Agora são <strong>${hora}</strong> horas da <strong>${turno}.</strong>`
     
-    manha.style.display = 'none'
-    tarde.style.display = 'block'
-    noite.style.display = 'none'
+    manha.setAttribute('src', 'imagens/tarde2.jpg')
 } else if(hora > 18 || hora <= 3){
     document.body.style.backgroundColor = '#212c33'
     turno = 'noite'
     res.innerHTML = `Agora são <strong>${hora}</strong> horas da <strong>${turno}.</strong>`
 
-    manha.style.display = 'none'
-    tarde.style.display = 'none'
-    noite.style.display = 'block'
+   manha.setAttribute('src', 'imagens/noite.jpg')
 } else{
     alert('Dados inquerentes, verifique-os e tente novante.')
 }
--- aula014/js/script.js
+++ aula014/js/script.js
@@ -1,11 +1,63 @@
 function calcular(){
+    var data =new Date()
+    var ano = data.getFullYear()
+    
     var idade = document.getElementById('idade')
     var sexo = document.getElementById('sex')
     var res = document.getElementById('res')
     var num = Number(idade.value)
+    var fsex = document.getElementsByName('radsex')
+
+    var foto = document.getElementById('foto')
+    var img = document.getElementById('img')
+    if (num.valueOf() == 0 || num > Number(ano.valueOf())){
+        alert('Verifique seus dados e tente novamente!')
+    }else{
+        soma = Number(ano.valueOf()) - num 
+        res.style.fontSize = '1.1em'
+        res.style.textAlign = 'center'
+
+
+        var genero = ''
+        var lig = ''
+
+        img.style.display = 'block'
+        if(fsex[0].checked){
+            genero = 'Homem'
+            lig = 'um'
+
+            if(soma >= 0 && soma <= 13){
+                img.style.display = 'block'
+                foto.setAttribute('src', 'imagens/criança.jpg')
+            }else if(soma <= 25){
+                img.style.display = 'block'
+                foto.setAttribute('src', 'imagens/jovem.jpg')
+            }else if(soma < 60 ){
+                img.style.display = 'block'
+                foto.setAttribute('src', 'imagens/adulto.jpg')
+            }else if(soma > 59){
+                img.style.display = 'block'
+                foto.setAttribute('src', 'imagens/idoso.jpg')
+            }
+        } else{
+            genero = 'Mulher'
+            lig = 'uma'
+
+            if(soma >= 0 && soma <= 13){
+                foto.setAttribute('src', 'imagens/criançafem.jpg')
+            }else if(soma <= 25){
+                foto.setAttribute('src', 'imagens/jovemfem.jpg')
+            }else if(soma < 60 ){
+                foto.setAttribute('src', 'imagens/adulta.jpg')
+            }else if(soma > 59){
+                foto.setAttribute('src', 'imagens/idosa.jpg')
+            }
+        }
+        
+        res.innerHTML = `Detectamos ${lig} ${genero} com <strong>${soma}</strong> anos!`
+
+    }
+
     
-    soma = 2024 - num 
-    res.style.fontSize = '1.1em'
-    res.style.textAlign = 'center'
-    res.innerHTML = `Você tem ${soma} anos de idade!`
+
 }
