c8799488eb28227649f113eb447776f71f28e929
thalistornquist <thalistornquist@gmail.com>
2024-09-16 19:47:31 -0300

    chained to geder

---
+++ aula013/repetições.js
@@ -0,0 +1,13 @@
+var n = 1
+/* TESTE LOGICO PRIMEIRO
+while(n <= 10){
+    console.log(`oi numero: ${n}`)
+    n++
+}
+*/
+
+//TESTE LOGICO NO FIM
+do {
+    console.log(`oi numero: ${n}`)
+    n++
+} while(n <=10)
