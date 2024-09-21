ea5af90ef5bc067be091fde6d7325b75d2e0d214
thalistornquist <thalistornquist@gmail.com>
2024-09-21 14:52:46 -0300

    inicio ex2a14

---
+++ aula014/ex1-aula015/js/node_7329f0ce30065.tmp.js
@@ -0,0 +1,8 @@
+
+let num = 2
+
+for(x = 1; x <= 10; x++ ){
+    for(z = num; z != ''; z += num){
+        console.log(z)
+    }
+}
---
+++ aula014/ex1-aula015/js/script.js
@@ -0,0 +1,9 @@
+function tabuada(){
+    let n = document.getElementById('txtn')
+    
+    let num = Number(n.value)
+    for(x = 1; x <= 10; x++){
+        
+    }
+
+}
