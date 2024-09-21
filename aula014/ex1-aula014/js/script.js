dd4053cf40b30c8506459da46998126c22c8b854
thalistornquist <thalistornquist@gmail.com>
2024-09-21 19:56:59 -0300

    fim exerciocio de repeticaso

--- aula014/ex1-aula014/js/script.js
+++ aula014/ex1-aula014/js/script.js
@@ -1,17 +1,18 @@
 function contar(){
     let i = document.getElementById('inicio')
     let f = document.getElementById('fim')
-    let p = document.getElementById('passo')
-    let inicio = Number(i.value)
-    let fim = Number(f.value)
-    let passo = Number(p.value) 
+    let p = document.getElementById('passo') 
     let res = document.getElementById('res')
 
-    if(inicio == fim){
+    if(inicio.value == ''|| fim.value == ''){
         
         alert('[ERRO] Verifique os dados!')
 
     }else{
+        let inicio = Number(i.value)
+        let fim = Number(f.value)
+        let passo = Number(p.value)
+
         res.innerHTML = `Contando: `
         if(passo < 1){
             passo = 1
@@ -29,9 +30,6 @@ function contar(){
         }
         
         res.innerHTML += `\u{1F3C1}`
-
-      
-    
            
     }
 
--- aula014/ex1-aula015/js/script.js
+++ aula014/ex1-aula015/js/script.js
@@ -1,9 +1,20 @@
 function tabuada(){
     let n = document.getElementById('txtn')
-    
+    let tab = document.getElementById('seltab')
     let num = Number(n.value)
-    for(x = 1; x <= 10; x++){
-        
+
+    tab.innerHTML = ''
+    if(n.value == ''){
+        alert('Por favor digite um número!')
+    }else{
+        for(x = 1; x <= 10; x++){
+        tab.style.display = 'block'
+        let item = document.createElement('option')
+        item.text = `${num} x ${x} = ${num*x}`
+        item.value = `tab${x}`
+        tab.appendChild(item)
     }
+    }
+    
 
 }
