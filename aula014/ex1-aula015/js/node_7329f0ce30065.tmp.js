13ca5d3f6b4bfe921008a1908456bb03bf7295a4
thalistornquist <thalistornquist@gmail.com>
2024-09-26 14:37:59 -0300

    algum bug

--- aula014/ex1-aula015/js/node_7329f0ce30065.tmp.js
+++
@@ -1,8 +0,0 @@
-
-let num = 2
-
-for(x = 1; x <= 10; x++ ){
-    for(z = num; z != ''; z += num){
-        console.log(z)
-    }
-}
---
+++ aula016/exercicios/ex1-aula16/js/script.js
@@ -0,0 +1,16 @@
+function adicionar(){
+    let num = document.getElementById('txtn')
+    let n = Number(num.value)
+    let sel = document.getElementById('selnum')
+
+
+    if (n > 0 && n <= 100){
+
+    } else {
+        alert('numero invalido!')
+    }
+}
+
+function finalizar(){
+
+}
