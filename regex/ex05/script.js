fdeaf2a85d6b7eb016e4491fde5dab21937c860b
thalistornquist <thalistornquist@gmail.com>
2024-10-18 18:27:31 -0300

    fim rergex

--- regex/ex05/script.js
+++ regex/ex05/script.js
@@ -1,12 +1,13 @@
 let str = `https://www.google.com/search?q=quantas+kcal+tem+uma+banana&rlz=1C1GCEA_enBR1106BR1106&oq=quantas+kcal+tem+uma+banana`
-let regex = /\?(\w+)=([\w+]+)&(\w+)=(\w+)&(\w+)=\2/g
+let regex = /[?&](\w+)=([\w+]+)/g
 
 
-let teste = regex.exec(str)
+let array
+let param = {}
 
-console.log(teste)
-
-let all = {
-    q: teste,
-    rlz: 
+while (array = regex.exec(str)){
+    param[array[1]] = array[2]
 }
+
+
+console.log(param)
